65dd2c679fa0838f2500ec37f9013d40dd3d0442
Jamie Le <jamie_le@mckinsey.com>
2023-10-01 21:31:14 +0700

    fix: update get book by id handler

--- lesson3-4/lesson3-4-sample-api/src/routers/v1/mock.routers.js
+++ lesson3-4/lesson3-4-sample-api/src/routers/v1/mock.routers.js
@@ -8,7 +8,7 @@ const router = Router();
 
 router.get('/', validate(bookValidator.getBookByAuthor), catchAsync(controller.getBookHandler));
 router.get('/authors', catchAsync(controller.getAuthorsHandler));
-router.get('/:id/:isLike', validate(bookValidator.updateLikeStatus), catchAsync(controller.updateLikeStatusOfBookHandler));
+router.get('/:id/:isLike', catchAsync(controller.updateLikeStatusOfBookHandler));
 router.get('/:id/', validate(bookValidator.getBookById), catchAsync(controller.getBookByIdHandler));
 
 module.exports = { router };
